3e25addf963b126093adf356ff87b54380be47b4
Nsubuga Mike <nsubugamike021@gmail.com>
2020-06-27 10:57:00 +0300

    Revert "Praisy erick"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +7,4 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'Telesim-Website';
-  constructor(public router: Router) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopNavbarComponent } from './Components/top-navbar/top-navbar.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { FrontComponent } from './Components/front/front.component';
-import { MainComponent } from './Components/main/main.component';
-import { CoursesComponent } from './Components/courses/courses.component';
-import { ServicesComponent } from './Components/services/services.component';
-import { AccessibleComponent } from './Components/accessible/accessible.component';
-import { CommunityComponent } from './Components/community/community.component';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopNavbarComponent,
-    FooterComponent,
-    FrontComponent,
-    MainComponent,
-    AccessibleComponent,
-    CoursesComponent,
-    ServicesComponent,
-    CommunityComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
